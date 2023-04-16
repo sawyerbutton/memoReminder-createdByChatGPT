b6a7f00bb9d7ae0711e0756e353699b616dc8a79
sawyerbutton <linghui92liu@gmail.com>
2023-04-16 09:18:44 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { MemoComponent } from './memo/memo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
